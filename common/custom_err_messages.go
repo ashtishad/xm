3ae349fb2a23cf756cf6564e36346d0b1112f129
Ashef Tishad <ashef.swe@gmail.com>
2024-09-27 18:03:47 +0600

    Added api request timeouts and custom error messages for consistency.

--- common/custom_err_messages.go
+++ common/custom_err_messages.go
@@ -1,9 +1,11 @@
 package common
 
 const (
+	ErrUnexpectedServer   = "unexpected server error occurred"
 	ErrInvalidRequest     = "failed to validate request"
 	ErrUnexpectedDatabase = "unexpected database error"
 	ErrTXBegin            = "failed to begin transaction"
 	ErrTXRollback         = "failed to rollback transaction"
 	ErrTxCommit           = "failed to commit transaction"
+	ErrIncorrectPassword  = "incorrect password"
 )
